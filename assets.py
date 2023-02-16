248cd62c283149113042413255ab0698e7226a77
Berliner <57003925+Berliner187@users.noreply.github.com>
2023-02-16 21:24:01 +0300

    Добавлен расчет кредита

--- assets.py
+++ assets.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 
-class AccumulationAccounts:
+class AssetCalculation:
     """
-        Расчет дохода по вкладу
+        Класс расчетных операций по вкладу и накопительному счету
         self.initial_investment_amount - сумма первоначальных вложений
+        self.annual_rate - годовая ставка, в процентах
     """
     def __init__(self, amount, annual_rate):
         self.investment_amount = amount
@@ -13,18 +14,14 @@ class AccumulationAccounts:
     @staticmethod
     def get_number_days_in_year():
         year_now = datetime.now().year
-        if year_now % 4 == 0:
-            return 366
-        else:
-            return 365
+        if year_now % 4 == 0: return 366
+        else: return 365
 
-    # Расчет простого процента
+    # <--- Расчет простых процентов --->
     def calculation_capitalization(self, number_of_deposit_days):
-        return (self.investment_amount *
-                self.annual_rate * number_of_deposit_days /
-                self.get_number_days_in_year()) / 100
+        return (self.investment_amount * self.annual_rate * number_of_deposit_days / self.get_number_days_in_year()) / 100
 
-    # Расчет сложных процентов
+    # <--- Расчет сложных процентов --->
     # Ежедневная капитализация
     def calculation_daily_capitalization(self, number_of_deposit_days):
         N = self.annual_rate / 100
@@ -39,4 +36,22 @@ class AccumulationAccounts:
         return round(capitalization - self.investment_amount, 2)
 
 
-# print(AccumulationAccounts(25500, 6).calculation_monthly_capitalization(6))
+class PassiveCalculation:
+    def __init__(self, amount, annual_rate, term):
+        self.amount = amount
+        self.annual_rate = annual_rate
+        self.term = term
+
+    # <--- Аннуитетный кредит --->
+    # Расчет ежемесячного платежа
+    def monthly_payment(self):
+        annual_rate = self.annual_rate / (100 * 12)
+        return round(self.amount * (annual_rate / (1 - (1 + annual_rate) ** -self.term)), 2)
+
+    def total_overpayment(self):
+        return round((self.monthly_payment() * self.term) - self.amount, 2)
+
+
+# print(AssetCalculation(59500, 6).calculation_capitalization(30))
+print(PassiveCalculation(20_000, 12, 36).monthly_payment())
+print(PassiveCalculation(20_000, 12, 36).total_overpayment())
